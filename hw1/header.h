4ca29f814808748b8f9a0037f785cca9061677c6
KEG <110787383+kgoggio@users.noreply.github.com>
2023-08-30 13:19:10 -0500

    Add files via upload

---
+++ hw1/header.h
@@ -0,0 +1,27 @@
+/*-------------------------------------------------------------------------*
+ *---									---*
+ *---		header.h						---*
+ *---									---*
+ *---	----	----	----	----	----	----	----	----	---*
+ *---									---*
+ *---	Version 1a					Joseph Phillips	---*
+ *---									---*
+ *-------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define		MAX_LINE	256
+
+#define		RANGE_LOWEST	0
+
+#define		RANGE_HIGHEST	32767
+
+#define		MIN_NUM_NUMBERS	0
+
+#define		MAX_NUM_NUMBERS	0x40000000
+
+void countWithList(int);
+void countWithTree(int);
+int getNextNumber();
